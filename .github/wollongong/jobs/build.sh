8f267e67e957eae5131d3785977653f9bf04db32
Joshua M. Clulow <jmc@oxide.computer>
2021-10-04 01:43:48 -0700

    try out illumos CI

---
+++ .github/wollongong/jobs/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#:
+#: name = "build / illumos"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "stable"
+#: output_rules = [
+#:	"/work/awl.*",
+#: ]
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+rustc --version
+
+cargo build --release
+
+cp target/release/aws-wire-lengths /work/awl
+digest -a sha256 /work/awl > /work/awl.sha256.txt
+gzip /work/awl
+digest -a sha256 /work/awl.gz > /work/awl.gz.sha256.txt
