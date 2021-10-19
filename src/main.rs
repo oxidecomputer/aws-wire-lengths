3522223a61a85ebcb588ce5ead4fce2e77d870b6
Joshua M. Clulow <jmc@oxide.computer>
2021-10-18 23:14:24 -0700

    "key ls", "key create", "key rm", for SSH key management

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ use s3::{
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::os::unix::fs::OpenOptionsExt;
 use std::str::FromStr;
 use std::time::Duration;
 use sts::AssumeRoleRequest;
@@ -47,6 +48,24 @@ trait RowExt {
 impl RowExt for Row {
     fn add_stror(&mut self, n: &str, v: &Option<String>, def: &str) {
         self.add_str(n, v.as_deref().unwrap_or(def));
+    }
+}
+
+trait AsFlag {
+    fn as_flag(&self, f: &str) -> String;
+}
+
+impl AsFlag for Option<bool> {
+    fn as_flag(&self, f: &str) -> String {
+        if let Some(val) = self {
+            if *val {
+                f
+            } else {
+                "-"
+            }
+        } else {
+            "-"
+        }.to_string()
     }
 }
 
@@ -1298,7 +1317,7 @@ async fn volumes(mut l: Level<Stuff>) -> Result<()> {
     l.add_column("size", 5, false);
     l.add_column("snapshot", 22, false);
     l.add_column("name", 24, false);
-    l.add_column("az", 12, false);
+    l.add_column("az", 14, false);
 
     let a = no_args!(l);
     let mut t = a.table();
@@ -1852,6 +1871,113 @@ async fn do_image(mut l: Level<Stuff>) -> Result<()> {
     sel!(l).run().await
 }
 
+async fn do_key(mut l: Level<Stuff>) -> Result<()> {
+    l.cmda("list", "ls", "list SSH keys", cmd!(do_key_ls))?;
+    l.cmd("create", "create new SSH key", cmd!(do_key_create))?;
+    l.cmda("destroy", "rm", "destroy an SSH key", cmd!(do_key_rm))?;
+
+    sel!(l).run().await
+}
+
+async fn do_key_rm(mut l: Level<Stuff>) -> Result<()> {
+    l.usage_args(Some("KEYNAME..."));
+
+    let a = args!(l);
+    let s = l.context();
+
+    if a.args().is_empty() {
+        bail!("specify which key name(s) to delete");
+    }
+
+    for name in a.args().iter() {
+        s.ec2()
+            .delete_key_pair(ec2::DeleteKeyPairRequest {
+                key_name: Some(name.to_string()),
+                ..Default::default()
+            })
+            .await?;
+        println!("deleted {}", name);
+    }
+
+    Ok(())
+}
+
+async fn do_key_create(mut l: Level<Stuff>) -> Result<()> {
+    l.usage_args(Some("KEYNAME"));
+    l.reqopt("o", "", "output file name for created key", "PEMFILE");
+
+    let a = args!(l);
+    let s = l.context();
+
+    if a.args().len() != 1 {
+        bail!("just one key name");
+    }
+
+    /*
+     * Open the target file without overwriting an existing file.
+     */
+    let path = a.opts().opt_str("o").unwrap();
+    let mut f = std::fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .mode(0o600)
+        .open(&path)?;
+
+    let key_name = a.args().get(0).unwrap().to_string();
+
+    let res = s
+        .ec2()
+        .create_key_pair(ec2::CreateKeyPairRequest {
+            key_name,
+            ..Default::default()
+        })
+        .await?;
+
+    if let Some(mat) = res.key_material.as_deref() {
+        f.write_all(mat.as_bytes())?;
+        f.flush()?;
+    } else {
+        bail!("no key material in response: {:#?}", res);
+    }
+
+    if let Some(fp) = res.key_fingerprint.as_deref() {
+        eprintln!("fingerprint = {}", fp);
+    }
+
+    Ok(())
+}
+
+async fn do_key_ls(mut l: Level<Stuff>) -> Result<()> {
+    l.add_column("name", 20, true);
+    l.add_column("fingerprint", 59, true);
+    l.add_column("id", 21, false);
+
+    let a = no_args!(l);
+    let mut t = a.table();
+    let s = l.context();
+
+    let res = s
+        .ec2()
+        .describe_key_pairs(ec2::DescribeKeyPairsRequest {
+            ..Default::default()
+        })
+        .await?;
+
+    let x = Vec::new();
+    for kp in res.key_pairs.as_ref().unwrap_or(&x) {
+        let mut r = Row::default();
+
+        r.add_stror("id", &kp.key_pair_id, "?");
+        r.add_stror("name", &kp.key_name, "-");
+        r.add_stror("fingerprint", &kp.key_fingerprint, "-");
+
+        t.add_row(r);
+    }
+
+    print!("{}", t.output()?);
+
+    Ok(())
+}
 
 async fn do_sg(mut l: Level<Stuff>) -> Result<()> {
     l.cmda("list", "ls", "list security groups", cmd!(do_sg_ls))?;
@@ -1988,6 +2114,7 @@ async fn main() -> Result<()> {
         cmd!(do_role),
     )?;
     l.cmd("sg", "security group management", cmd!(do_sg))?;
+    l.cmd("key", "SSH key management", cmd!(do_key))?;
     l.cmd("subnet", "subnet management", cmd!(do_subnet))?;
     /*
      * XXX These are used in some scripts, so leave them (but hidden) for now.
