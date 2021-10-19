c38517d9480bcb3be70d72339879602932285d24
Joshua M. Clulow <jmc@oxide.computer>
2021-10-18 23:13:55 -0700

    add "subnet ls" to list security groups

--- src/main.rs
+++ src/main.rs
@@ -1905,6 +1905,70 @@ async fn do_sg_ls(mut l: Level<Stuff>) -> Result<()> {
     Ok(())
 }
 
+async fn do_subnet(mut l: Level<Stuff>) -> Result<()> {
+    l.cmda("list", "ls", "list subnets", cmd!(do_subnet_ls))?;
+
+    sel!(l).run().await
+}
+
+async fn do_subnet_ls(mut l: Level<Stuff>) -> Result<()> {
+    l.add_column("id", 24, true);
+    l.add_column("name", 20, true);
+    l.add_column("cidr", 18, true);
+    l.add_column("az", 14, false);
+    l.add_column("vpc", 21, false);
+    l.add_column("flags", 5, true);
+    l.add_column("avail", 5, true);
+
+    let a = no_args!(l);
+    let mut t = a.table();
+    let s = l.context();
+
+    let res = s
+        .ec2()
+        .describe_subnets(ec2::DescribeSubnetsRequest {
+            ..Default::default()
+        })
+        .await?;
+
+    let x = Vec::new();
+    for sn in res.subnets.as_ref().unwrap_or(&x) {
+        /*
+         * Find the name tag value:
+         */
+        let nametag = if let Some(tags) = sn.tags.as_ref() {
+            tags.iter()
+                .find(|t| t.key.as_deref() == Some("Name"))
+                .and_then(|t| t.value.as_deref())
+                .map(|s| s.to_string())
+        } else {
+            None
+        };
+
+        let flags = [
+            sn.map_public_ip_on_launch.as_flag("P"),
+            sn.default_for_az.as_flag("D"),
+        ]
+        .join("");
+
+        let mut r = Row::default();
+
+        r.add_stror("id", &sn.subnet_id, "?");
+        r.add_stror("vpc", &sn.vpc_id, "-");
+        r.add_stror("cidr", &sn.cidr_block, "-");
+        r.add_stror("name", &nametag, "-");
+        r.add_stror("az", &sn.availability_zone, "-");
+        r.add_str("flags", &flags);
+        r.add_u64("avail", sn.available_ip_address_count.unwrap_or(0) as u64);
+
+        t.add_row(r);
+    }
+
+    print!("{}", t.output()?);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut l = Level::new("aws-wire-lengths", Stuff::default());
@@ -1924,6 +1988,7 @@ async fn main() -> Result<()> {
         cmd!(do_role),
     )?;
     l.cmd("sg", "security group management", cmd!(do_sg))?;
+    l.cmd("subnet", "subnet management", cmd!(do_subnet))?;
     /*
      * XXX These are used in some scripts, so leave them (but hidden) for now.
      */
