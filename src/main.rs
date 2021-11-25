cb82bf4fef2ed7400bd256bc4d8437f10fb3155c
Joshua M. Clulow <jmc@oxide.computer>
2021-11-24 17:57:11 -0800

    "type ls", to list instance types

--- src/main.rs
+++ src/main.rs
@@ -1163,7 +1163,7 @@ async fn get_instance_x(
                     tags: inst
                         .tags
                         .as_ref()
-                        .map(|o| o.iter().cloned().collect())
+                        .map(|o| o.to_vec())
                         .unwrap_or_default(),
                 });
             }
@@ -2525,6 +2525,147 @@ async fn do_config(mut l: Level<Stuff>) -> Result<()> {
     sel!(l).run().await
 }
 
+async fn do_type(mut l: Level<Stuff>) -> Result<()> {
+    l.cmda("list", "ls", "list instance types", cmd!(do_type_ls))?;
+
+    sel!(l).run().await
+}
+
+async fn do_type_ls(mut l: Level<Stuff>) -> Result<()> {
+    l.optflag("X", "", "only 64-bit x86 instances");
+    l.optflag("A", "", "only 64-bit ARM instances");
+    l.optflag("C", "", "only current generation instances");
+    l.optopt("n", "", "instance type name (supports wildcards)", "NAME");
+    l.mutually_exclusive(&[("X", "A")]);
+
+    l.add_column("name", 20, true);
+    l.add_column("arch", 7, true);
+    l.add_column("vcpu", 4, true);
+    l.add_column("ram", 9, true);
+    l.add_column("flags", 5, true);
+
+    let a = no_args!(l);
+    let mut t = a.table();
+    let s = l.context();
+
+    let mut filters = Vec::new();
+    if a.opts().opt_present("X") {
+        filters.push(ec2::Filter {
+            name: Some("processor-info.supported-architecture".into()),
+            values: Some(vec!["x86_64".into()]),
+        });
+    }
+    if a.opts().opt_present("A") {
+        filters.push(ec2::Filter {
+            name: Some("processor-info.supported-architecture".into()),
+            values: Some(vec!["arm64".into()]),
+        });
+    }
+    if a.opts().opt_present("C") {
+        filters.push(ec2::Filter {
+            name: Some("current-generation".into()),
+            values: Some(vec!["true".into()]),
+        });
+    }
+    if let Some(name) = a.opts().opt_str("n") {
+        filters.push(ec2::Filter {
+            name: Some("instance-type".into()),
+            values: Some(vec![name]),
+        });
+    }
+
+    let mut types = Vec::new();
+    let mut tok = None;
+    loop {
+        let res = s
+            .ec2()
+            .describe_instance_types(ec2::DescribeInstanceTypesRequest {
+                filters: Some(filters.clone()),
+                next_token: tok,
+                ..Default::default()
+            })
+            .await?;
+
+        if let Some(it) = res.instance_types {
+            types.extend(it);
+        }
+
+        if let Some(ntok) = res.next_token {
+            tok = Some(ntok);
+        } else {
+            break;
+        }
+    }
+
+    for typ in types {
+        let mut r = Row::default();
+
+        let arch = if let Some(pi) = &typ.processor_info {
+            if let Some(arch) = &pi.supported_architectures {
+                if arch.len() != 1 {
+                    if arch.contains(&"i386".to_string())
+                        && arch.contains(&"x86_64".to_string())
+                    {
+                        Some("x86".to_string())
+                    } else {
+                        bail!("weird instance type? {:?}", arch);
+                    }
+                } else if arch[0] == "x86_64_mac" {
+                    Some("x86_mac".to_string())
+                } else if arch[0] == "arm64" {
+                    Some("arm".to_string())
+                } else if arch[0] == "x86_64" {
+                    Some("x86".to_string())
+                } else if arch[0] == "i386" {
+                    bail!("386 only? {:?}", typ.instance_type);
+                } else {
+                    Some(arch[0].to_string())
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        let memory_bytes = typ
+            .memory_info
+            .map(|mi| mi.size_in_mi_b)
+            .flatten()
+            .map(|megs| (megs as u64) * 1024 * 1024)
+            .unwrap_or(0);
+
+        let vcpu = typ
+            .v_cpu_info
+            .as_ref()
+            .map(|ci| ci.default_v_cpus)
+            .flatten()
+            .map(|nvcpus| nvcpus as u64)
+            .unwrap_or(0);
+
+        let ena = typ
+            .network_info
+            .map(|ni| ni.ena_support)
+            .flatten()
+            .map(|ena| ena == "supported" || ena == "required");
+
+        let flags =
+            [typ.current_generation.as_flag("C"), ena.as_flag("E")].join("");
+
+        r.add_stror("name", &typ.instance_type, "?");
+        r.add_stror("arch", &arch, "-");
+        r.add_bytes("ram", memory_bytes);
+        r.add_u64("vcpu", vcpu);
+        r.add_str("flags", &flags);
+
+        t.add_row(r);
+    }
+
+    print!("{}", t.output()?);
+
+    Ok(())
+}
+
 async fn do_subnet(mut l: Level<Stuff>) -> Result<()> {
     l.cmda("list", "ls", "list subnets", cmd!(do_subnet_ls))?;
 
@@ -2615,6 +2756,7 @@ async fn main() -> Result<()> {
         "manage account- or region-level configuration",
         cmd!(do_config),
     )?;
+    l.cmd("type", "instance type management", cmd!(do_type))?;
     /*
      * XXX These are used in some scripts, so leave them (but hidden) for now.
      */
