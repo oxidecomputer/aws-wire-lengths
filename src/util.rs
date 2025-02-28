69e7c1cbfb1e7b8f2801db2a95b91dea88d06982
Joshua M. Clulow <jmc@oxide.computer>
2025-02-28 14:39:07 -0800

    print list of object IDs if fuzzy lookup matches more than one

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2025 Oxide Computer Company
+ */
+
 use anyhow::{bail, Result};
 use base64::Engine;
 use chrono::prelude::*;
@@ -82,18 +86,90 @@ impl TagExtractor for Option<Vec<aws_sdk_ec2::types::Tag>> {
     }
 }
 
+pub trait DisplayId {
+    fn display_id(&self) -> String;
+}
+
+impl DisplayId for aws_sdk_ec2::types::Volume {
+    fn display_id(&self) -> String {
+        self.volume_id().expect("volume_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::Image {
+    fn display_id(&self) -> String {
+        self.image_id().expect("image_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::Subnet {
+    fn display_id(&self) -> String {
+        self.subnet_id().expect("subnet_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::RouteTable {
+    fn display_id(&self) -> String {
+        self.route_table_id().expect("route_table_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::SecurityGroup {
+    fn display_id(&self) -> String {
+        self.group_id().expect("group_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::Vpc {
+    fn display_id(&self) -> String {
+        self.vpc_id().expect("vpc_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::Address {
+    fn display_id(&self) -> String {
+        self.allocation_id().expect("allocation_id").to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::InternetGateway {
+    fn display_id(&self) -> String {
+        self.internet_gateway_id()
+            .expect("internet_gateway_id")
+            .to_string()
+    }
+}
+
+impl DisplayId for aws_sdk_ec2::types::NatGateway {
+    fn display_id(&self) -> String {
+        self.nat_gateway_id().expect("nat_gateway_id").to_string()
+    }
+}
 pub fn one_ping_only<T>(
     noun: &str,
     filter: &str,
     v: Option<Vec<T>>,
-) -> Result<T> {
+) -> Result<T>
+where
+    T: DisplayId,
+{
     if let Some(mut v) = v {
         if v.len() == 1 {
             return Ok(v.pop().unwrap());
         }
 
         if v.len() > 1 {
-            bail!("more than one {} matched filter \"{}\"", noun, filter);
+            let ids = v
+                .iter()
+                .map(|v| v.display_id())
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            bail!(
+                "more than one {} matched filter \"{}\": {ids}",
+                noun,
+                filter,
+            );
         }
     }
 
